a3bc05b6033e0f1c5f9ea76c387520eaac848acb
David Crawford <DaveAldon@users.noreply.github.com>
2018-05-16 13:30:31 -0400

    Code cleanup, and added more documentation.

--- js/easyPDF.js
+++ js/easyPDF.js
@@ -1,101 +1,97 @@
 function easyPDF(base64, title) {
+  // HTML definition of dialog elements
+  var dialog = '<div id="pdfDialog" title="'+title+'">'+
+            			'<label>Page: </label><label id="pageNum"></label>'+
+            			'<canvas id="pdfview"></canvas>'+
+            		'</div>';
+  $(document.body).append(dialog);
 
-    var dialog = '<div id="pdfDialog" title="'+title+'">'+
-              			'<label>Page: </label><label id="pageNum"></label>'+
-              			'<canvas id="pdfview"></canvas>'+
-              		'</div>';
-    $(document.body).append(dialog);
-    var canvas = document.getElementById('pdfview');
+  // We need the javascript object of the canvas, not the jQuery reference
+  var canvas = document.getElementById('pdfview');
+  // Init page count
+	var page = 1;
+  // Dialog definition
+	$( "#pdfDialog" ).dialog({
+    // Moves controls to top of dialog
+		open: function (event, ui) {
+			$(this).before($(this).parent().find('.ui-dialog-buttonpane'));
+		},
+		width: ($(window).width() / 2),
+    modal: true,
+    position: {
+  		my: "top",
+    	at: "top",
+    	of: window,
+    	collision: "none"
+    },
+    buttons: {
+    	"Back": {
+        click: function () {
+        	RenderPDF(-1)
+        },
+        text: 'Previous',
+      },
+      "Next": {
+        click: function () {
+        	RenderPDF(1)
+        },
+        text: 'Next',
+      },
+      "Confirm": {
+        click: function () {
+          $(this).dialog("close");
+          $("#pdfDialog").remove()
+        },
+        text: 'Close',
+      }
+    }
+	});
 
-		var w = $(window).width() / 2;
-		var page = 1;
-		$( "#pdfDialog" ).dialog({
-			open: function (event, ui) {
-				$(this).before($(this).parent().find('.ui-dialog-buttonpane'));
-			},
-			width: w,
-	        modal: true,
-	        position: {
-        		my: "top",
-	        	at: "top",
-	        	of: window,
-	        	collision: "none"
-	        },
-	        buttons: {
-	        	"Back": {
-	                click: function () {
-	                	RenderPDF(base64,canvas, -1)
-	                },
-	                text: 'Back Page',
-	            },
-	            "Next": {
-	                click: function () {
-	                	RenderPDF(base64,canvas, 1)
-	                },
-	                text: 'Next Page',
-	            },
-	            "Confirm": {
-	                click: function () {
-	                	$('#pdfDialog').hide()
-	                	$('#pdfview').hide()
-	                    $(this).dialog("close");
-	                },
-	                text: 'Close',
-	            },
-	        }
-		});
-		$('#pageNum').text("1")
-	    RenderPDF(base64,canvas, 0)
-	    $('#pdfDialog').show()
-	    $('#pdfview').show()
+  // Init page number and the document
+	$('#pageNum').text(page);
+  RenderPDF(0);
 
-      function RenderPDF(_base64, element, pageNumber) {
-        console.log()
-	//base64 = //$('#OriginalHL7').html()
+  // PDF.js control
+  function RenderPDF(pageNumber) {
+  	var pdfData = atob(base64);
+  	pdfjsLib.disableWorker = true;
 
-	//base64 = base64.split('^PDF^^base64^')[1] + "";
-	//console.log(base64)
+  	// Get current global page number, defaults to 1
+  	displayNum = parseInt($('#pageNum').html())
+  	pageNumber = parseInt(pageNumber)
 
-	var pdfData = atob(_base64);
-	pdfjsLib.disableWorker = true;
-
-	// Get current global page number, defaults to 1
-	displayNum = parseInt($('#pageNum').html())
-	pageNumber = parseInt(pageNumber)
-	var loadingTask = pdfjsLib.getDocument({data: pdfData});
-	loadingTask.promise.then(function(pdf) {
-		// Gets total page length of pdf
-		size = pdf.numPages
-		// Handling for changing pages
-		if(pageNumber == 1) {
-			pageNumber = displayNum + 1;
-		}
-		if(pageNumber == -1) {
-			pageNumber = displayNum - 1;
-		}
-		if(pageNumber == 0) {
-			pageNumber = 1;
-		}
-		if(pageNumber > size || pageNumber < 1) {
-			throw "bad page number";
-		}
-		// Changes the cheeky global to our valid new page number
-		$('#pageNum').text(pageNumber)
-		pdf.getPage(pageNumber).then(function(page) {
-			var scale = 2.0;
-			var viewport = page.getViewport(scale);
-			var canvas = element;
-			var context = canvas.getContext('2d');
-
-			canvas.height = viewport.height;
-			canvas.width = viewport.width;
-
-			var renderContext = {
-			  canvasContext: context,
-			  viewport: viewport
-			};
-			page.render(renderContext);
-		});
-	}).catch(e => {});
-}
+  	var loadingTask = pdfjsLib.getDocument({data: pdfData});
+  	loadingTask.promise.then(function(pdf) {
+  		// Gets total page length of pdf
+  		size = pdf.numPages
+  		// Handling for changing pages
+  		if(pageNumber == 1) {
+  			pageNumber = displayNum + 1;
+  		}
+  		if(pageNumber == -1) {
+  			pageNumber = displayNum - 1;
+  		}
+  		if(pageNumber == 0) {
+  			pageNumber = 1;
+  		}
+      // If the requested page is outside the document bounds
+  		if(pageNumber > size || pageNumber < 1) {
+  			throw "bad page number";
+  		}
+  		// Changes the cheeky global to our valid new page number
+  		$('#pageNum').text(pageNumber)
+  		pdf.getPage(pageNumber).then(function(page) {
+  			var scale = 2.0;
+  			var viewport = page.getViewport(scale);
+  			var context = canvas.getContext('2d');
+  			canvas.height = viewport.height;
+  			canvas.width = viewport.width;
+  			var renderContext = {
+  			  canvasContext: context,
+  			  viewport: viewport
+  			};
+  			page.render(renderContext);
+  		});
+  	}).catch(e => {});
+  }
 }
